2e69871cf888bc42eba4e75e53f827b78214466b
dimasazonov98-netizen <dima.sazonov.98@gmail.com>
2026-09-25 23:25:41 +0300

    Добавить app/src/main/java/com/dmitry/zarplata/MainActivity.java

---
+++ app/src/main/java/com/dmitry/zarplata/MainActivity.java
@@ -0,0 +1,36 @@
+package com.dmitry.zarplata;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Color;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(7,17,31));
+        getWindow().setNavigationBarColor(Color.rgb(5,11,20));
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
